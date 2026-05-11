89b0078966f329fe5cd0fd161ae4d64d7db0fa9d
Xavier-btt <arthur.xavier09@escola.pr.gov.br>
2026-05-11 14:41:37 +0000

    ok

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 for (let i =0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add(".ativo")
+    }
 console.log(botoes);
 }
