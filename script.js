f4a106305a4b9a278afc618e87421b7d26f210ed
Xavier-btt <arthur.xavier09@escola.pr.gov.br>
2026-05-18 15:07:28 +0000

    feito

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = documentos.querySelectorAll(".aba-conteudo");
 for (let i =0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length)
-        botoes[i].classList.add(".ativo")
+        for(let j=0; j<botoes.length){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add(".ativo");
+        textos[i].classList.add("ativo");
     }
 console.log(botoes);
 }
