c79f137672b4b1745ccf7fae4fa4ab7b49e28248
Xavier-btt <arthur.xavier09@escola.pr.gov.br>
2026-05-25 14:39:35 +0000

    ...

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
+// [PARTE 1 LIBERADA] - Controle de Abas
 const botoes = document.querySelectorAll(".botao");
-const textos = documentos.querySelectorAll(".aba-conteudo");
-for (let i =0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length){
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function() {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add(".ativo");
+        botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-console.log(botoes);
 }
+// [PARTE 2 LIBERADA] - Definição dos Alvos
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo2 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo3 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo4 = new Date("2026-12-28T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+];
+// [PARTE 3 LIBERADA] - Inicialização dos Contadores
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+// [PARTE 4 LIBERADA] - Função de Cálculo Matemático
+function calculaTempo(tempoObjetivo1) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias "
+        + horas + " horas "
+        + minutos + " minutos "
+        + segundos + " segundos "; }
